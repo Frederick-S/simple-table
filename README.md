7f362fba24a1cd8d991704ba9b94a7038ffaab32
Frederick-S <mao_xiaodan@hotmail.com>
2017-08-06 22:34:00 +0800

    Init

---
+++ README.md
@@ -0,0 +1,2 @@
+# simple-table
+A very very very basic terminal table.
---
+++ tests/simple_table_test.py
@@ -0,0 +1,16 @@
+import unittest
+from simple_table.simple_table import SimpleTable
+
+
+table_text = '''| Name | Age | City |\n| Tom  | 12  | AAA  |
+| Kate | 13  | BBB  |\n'''
+
+
+class TestSimpleTable(unittest.TestCase):
+    def test_render(self):
+        table = SimpleTable()
+        table.set_headers(['Name', 'Age', 'City'])
+        table.add_row(['Tom', '12', 'AAA'])
+        table.add_row(['Kate', '13', 'BBB'])
+
+        self.assertEqual(table_text, str(table))
